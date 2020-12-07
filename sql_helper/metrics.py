cb977598b04940b02f0f230545514c557d82b8dd
Blue <blue@nqn.blue>
2020-12-07 19:10:38 +0000

    Add metrics helper

---
+++ sql_helper/metrics.py
@@ -0,0 +1,23 @@
+import time
+from prometheus_client import Histogram
+
+
+async def sql_wrapper(namespace: str):
+    sql_request = Histogram(
+        "sql_request_total",
+        "SQL Requests",
+        ["query"],
+        namespace=namespace
+    )
+
+    def middle(execute):
+        async def execute_wrapper(query, *args, **kwargs):
+            start_time = time.time()
+            try:
+                return await execute(query, *args, **kwargs)
+            finally:
+                end_time = time.time() - start_time
+                sql_request.labels(query=query).observe(end_time)
+
+        return execute_wrapper
+    return middle
