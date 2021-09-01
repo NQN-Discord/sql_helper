e48e5a2aada4ba756862fe1f1b3186911e0b4228
Blue <blue@nqn.blue>
2021-09-01 12:59:43 +0100

    Add sentry metrics

--- sql_helper/metrics.py
+++ sql_helper/metrics.py
@@ -1,5 +1,6 @@
 import time
 from prometheus_client import Histogram
+from sentry_sdk import add_breadcrumb
 
 
 def sql_wrapper(namespace: str):
@@ -12,12 +13,22 @@ def sql_wrapper(namespace: str):
 
     def middle(execute):
         async def execute_wrapper(query, *args, **kwargs):
-            start_time = time.time()
+            start_time = time.perf_counter()
             try:
                 return await execute(query, *args, **kwargs)
             finally:
-                end_time = time.time() - start_time
-                sql_request.labels(query=query).observe(end_time)
+                total_time = time.perf_counter() - start_time
+                sql_request.labels(query=query).observe(total_time)
+
+                add_breadcrumb(
+                    message="Made SQL query",
+                    category="sql",
+                    data={
+                        "query": query,
+                        "params": kwargs.get("parameters"),
+                        "total_time": total_time
+                    }
+                )
 
         return execute_wrapper
     return middle
