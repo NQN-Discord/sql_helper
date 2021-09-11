18614357e2e000a4c802ecf4e9e9b7528fbe3076
Blue <blue@nqn.blue>
2021-09-11 14:16:01 +0100

    Fix personas bug

--- sql_helper/mixins/personas.py
+++ sql_helper/mixins/personas.py
@@ -24,10 +24,15 @@ class PersonasMixin(_PostgresConnection):
         if personas:
             return Persona(*personas[0])
 
-    async def create_persona(self, user_id: int, short_name: str):
+    async def create_persona(self, persona: Persona):
         await self.cur.execute(
-            "INSERT INTO personas (user_id, short_name, display_name) VALUES (%(user_id)s, %(short_name)s, %(short_name)s)",
-            parameters={"user_id": user_id, "short_name": short_name}
+            "INSERT INTO personas (user_id, short_name, display_name, avatar_url) VALUES (%(user_id)s, %(short_name)s, %(display_name)s, %(avatar_url)s)",
+            parameters={
+                "user_id": persona.user_id,
+                "short_name": persona.short_name,
+                "display_name": persona.display_name,
+                "avatar_url": persona.avatar_url
+            }
         )
 
     async def set_persona(self, original_name: str, persona: Persona):
