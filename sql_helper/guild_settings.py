e84adae20da16e9194070e88371b710f1fc00300
Blue <blue@nqn.blue>
2022-01-29 16:40:40 +0000

    Simplify guild settings data structure and use dataclasses

--- sql_helper/guild_settings.py
+++ sql_helper/guild_settings.py
@@ -1,64 +1,23 @@
 from typing import Optional
+from dataclasses import dataclass
 
 
+@dataclass()
 class GuildSettings:
-    __slots__ = [
-        "guild_id",
-        "prefix",
-        "nitro_role",
-        "boost_channel",
-        "boost_role",
-        "audit_channel",
-        "enable_stickers",
-        "enable_nitro",
-        "enable_replies",
-        "enable_masked_links",
-        "is_alias_server",
-        "locale",
-        "enable_pings",
-        "max_guildwide_emotes",
-        "enable_user_content",
-        "enable_personas",
-        "enable_dashboard_posting"
-    ]
-    
-    def __init__(
-            self,
-            guild_id,
-            prefix,
-            nitro_role,
-            boost_channel,
-            boost_role,
-            audit_channel,
-            enable_stickers,
-            enable_nitro,
-            enable_replies,
-            enable_masked_links,
-            is_alias_server,
-            locale,
-            enable_pings,
-            max_guildwide_emotes,
-            enable_user_content,
-            enable_personas,
-            enable_dashboard_posting
-    ):
-        self.guild_id = guild_id
-        self.prefix = prefix
-        self.nitro_role = nitro_role
-        self.boost_channel = boost_channel
-        self.boost_role = boost_role
-        self.audit_channel = audit_channel
-        self.enable_stickers = self._parse_boolean(enable_stickers)
-        self.enable_nitro = self._parse_boolean(enable_nitro)
-        self.enable_replies = self._parse_boolean(enable_replies)
-        self.enable_masked_links = self._parse_boolean(enable_masked_links)
-        self.enable_pings = self._parse_boolean(enable_pings)
-        self.is_alias_server = self._parse_boolean(is_alias_server, on_none=False)
-        self.locale = locale
-        self.max_guildwide_emotes = max_guildwide_emotes
-        self.enable_user_content = self._parse_boolean(enable_user_content)
-        self.enable_personas = self._parse_boolean(enable_personas, on_none=False)
-        self.enable_dashboard_posting = self._parse_boolean(enable_dashboard_posting)
-
-    def _parse_boolean(self, bool: Optional[bool], on_none: Optional[bool] = True):
-        return on_none if bool is None else bool
+    guild_id: int
+    prefix: str = "!"
+    locale: str = "en"
+    max_guildwide_emotes: int = 10
+    nitro_role: Optional[int] = None
+    boost_channel: Optional[int] = None
+    boost_role: Optional[int] = None
+    audit_channel: Optional[int] = None
+    enable_stickers: bool = True
+    enable_nitro: bool = True
+    enable_replies: bool = True
+    enable_masked_links: bool = True
+    is_alias_server: bool = False
+    enable_pings: bool = True
+    enable_user_content: bool = True
+    enable_personas: bool = False
+    enable_dashboard_posting: bool = True
--- sql_helper/mixins/guild_settings.py
+++ sql_helper/mixins/guild_settings.py
@@ -1,5 +1,6 @@
 from typing import Union, Optional, List, Tuple
 from discord import Guild
+from dataclasses import fields
 from ..guild_settings import GuildSettings
 
 from ..async_list import AsyncList, async_list
@@ -24,14 +25,14 @@ class GuildSettingsMixin(_PostgresConnection):
     @async_list
     async def guild_settings(self) -> AsyncList:
         await self.cur.execute(
-            "SELECT guild_id, prefix, nitro_role, boost_channel, boost_role, audit_channel, enable_stickers, enable_nitro, enable_replies, enable_masked_links, is_alias_server, locale, enable_pings, max_guildwide_emotes, enable_user_content, enable_personas, enable_dashboard_posting FROM guild_settings"
+            "SELECT guild_id, prefix, locale, max_guildwide_emotes, nitro_role, boost_channel, boost_role, audit_channel, enable_stickers, enable_nitro, enable_replies, enable_masked_links, is_alias_server, enable_pings, enable_user_content, enable_personas, enable_dashboard_posting FROM guild_settings"
         )
         results = await self.cur.fetchall()
         return [GuildSettings(*i) for i in results]
 
     async def get_guild_settings(self, guild_id: Union[Guild, int]) -> Optional[GuildSettings]:
         await self.cur.execute(
-            "SELECT guild_id, prefix, nitro_role, boost_channel, boost_role, audit_channel, enable_stickers, enable_nitro, enable_replies, enable_masked_links, is_alias_server, locale, enable_pings, max_guildwide_emotes, enable_user_content, enable_personas, enable_dashboard_posting FROM guild_settings WHERE guild_id=%(guild_id)s",
+            "SELECT guild_id, prefix, locale, max_guildwide_emotes, nitro_role, boost_channel, boost_role, audit_channel, enable_stickers, enable_nitro, enable_replies, enable_masked_links, is_alias_server, enable_pings, enable_user_content, enable_personas, enable_dashboard_posting FROM guild_settings WHERE guild_id=%(guild_id)s",
             parameters={"guild_id": guild_id}
         )
         results = await self.cur.fetchall()
@@ -53,13 +54,13 @@ class GuildSettingsMixin(_PostgresConnection):
         )
         return await self.cur.fetchall()
 
-    async def set_guild_settings(self, guild: GuildSettings):
+    async def set_guild_settings(self, guild_settings: GuildSettings):
         await self.cur.execute(
             "INSERT INTO guild_settings (guild_id, prefix, nitro_role, boost_channel, boost_role, audit_channel, enable_stickers, enable_nitro, enable_replies, enable_masked_links, is_alias_server, locale, enable_pings, max_guildwide_emotes, enable_user_content, enable_personas, enable_dashboard_posting)  VALUES "
             "(%(guild_id)s, %(prefix)s, %(nitro_role)s, %(boost_channel)s, %(boost_role)s, %(audit_channel)s, %(enable_stickers)s, %(enable_nitro)s, %(enable_replies)s, %(enable_masked_links)s, %(is_alias_server)s, %(locale)s, %(enable_pings)s, %(max_guildwide_emotes)s, %(enable_user_content)s, %(enable_personas)s, %(enable_dashboard_posting)s)"
             'ON CONFLICT (guild_id) DO UPDATE SET (prefix, nitro_role, boost_channel, boost_role, audit_channel, enable_stickers, enable_nitro, enable_replies, enable_masked_links, is_alias_server, "locale", enable_pings, max_guildwide_emotes, enable_user_content, enable_personas, enable_dashboard_posting) = '
             "(EXCLUDED.prefix, EXCLUDED.nitro_role, EXCLUDED.boost_channel, EXCLUDED.boost_role, EXCLUDED.audit_channel, EXCLUDED.enable_stickers, EXCLUDED.enable_nitro, EXCLUDED.enable_replies, EXCLUDED.enable_masked_links, EXCLUDED.is_alias_server, EXCLUDED.locale, EXCLUDED.enable_pings, EXCLUDED.max_guildwide_emotes, EXCLUDED.enable_user_content, EXCLUDED.enable_personas, EXCLUDED.enable_dashboard_posting)",
-            parameters={slot: getattr(guild, slot) for slot in guild.__slots__}
+            parameters={field.name: getattr(guild_settings, field.name) for field in fields(guild_settings)}
         )
 
     async def delete_guild_settings(self, guild_id: int):
