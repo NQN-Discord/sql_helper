8eccbdb9c3058ebdb71dbde2b661c1161a10e029
Blue <blue@nqn.blue>
2022-08-13 14:19:19 +0100

    Restart using `emojis_used.py`

--- sql_helper/mixins/emojis_used.py
+++ sql_helper/mixins/emojis_used.py
@@ -1,66 +1,27 @@
-from typing import List, Optional
+from datetime import datetime
+from typing import List, Tuple
 from discord import PartialEmoji
 from .._connection import _PostgresConnection
-from ..async_list import AsyncList, async_list
 
 
 class EmojisUsedMixin(_PostgresConnection):
-    async def add_used_emotes(self, guild_id: int, author_id: int, message_id: int, emotes: List[PartialEmoji], force_author: bool):
-        names = []
-        ids = []
-        animateds = []
-        for emote in emotes:
-            names.append(emote.name)
-            ids.append(emote.id)
-            animateds.append(emote.animated)
+    async def add_used_emotes(self, to_cache: List[Tuple[int, PartialEmoji, datetime]]):
+        params = {
+            "times": [],
+            "guild_ids": [],
+            "names": [],
+            "ids": [],
+            "animateds": [],
+        }
 
-        if force_author:
-            on_constraint_do = "UPDATE SET author_id = excluded.author_id"
-        else:
-            on_constraint_do = "NOTHING"
+        for guild_id, emote, time in to_cache:
+            params["guild_ids"].append(guild_id)
+            params["times"].append(time)
+            params["names"].append(emote.name)
+            params["ids"].append(emote.id)
+            params["animateds"].append(emote.animated)
 
         await self.cur.execute(
-            f"INSERT INTO emotes_used (guild_id, author_id, message_id, name, emote_id, animated) VALUES (%(guild_id)s, %(author_id)s, %(message_id)s, unnest(%(names)s), unnest(%(emote_ids)s), unnest(%(animateds)s)) ON CONFLICT ON CONSTRAINT emotes_used_pk DO {on_constraint_do}",
-            parameters={
-                "guild_id": guild_id,
-                "author_id": author_id,
-                "message_id": message_id,
-                "names": names,
-                "emote_ids": ids,
-                "animateds": animateds,
-            }
+            f"INSERT INTO emotes_used (time, guild_id, name, emote_id, animated) VALUES (unnest(%(times)s), unnest(%(guild_ids)s), unnest(%(names)s), unnest(%(ids)s), unnest(%(animateds)s))",
+            parameters=params
         )
-
-    async def get_used_guild_emote(self, guild_id: int, name: str, after: int) -> Optional[PartialEmoji]:
-        await self.cur.execute(
-            f"SELECT animated, \"name\", emote_id FROM emotes_used WHERE guild_id=%(guild_id)s AND name=%(name)s AND message_id > %(after)s ORDER BY message_id DESC LIMIT 1",
-            parameters={"guild_id": guild_id, "name": name, "after": after}
-        )
-
-        results = await self.cur.fetchall()
-        if results:
-            return PartialEmoji(animated=results[0][0], name=results[0][1].rstrip(" "), id=results[0][2])
-
-    @async_list
-    async def recently_used_guild_emotes(self, guild_id: int, count: int) -> AsyncList:
-        await self.cur.execute(
-            f"SELECT animated, \"name\", emote_id FROM emotes_used WHERE guild_id=%(guild_id)s ORDER BY message_id DESC LIMIT %(count)s",
-            parameters={"guild_id": guild_id, "count": count}
-        )
-        return await _get_emotes(self.cur)
-
-    @async_list
-    async def recently_used_user_emotes(self, user_id: int, count: int) -> AsyncList:
-        await self.cur.execute(
-            f"SELECT animated, \"name\", emote_id FROM emotes_used WHERE author_id=%(author_id)s ORDER BY message_id DESC LIMIT %(count)s",
-            parameters={"author_id": user_id, "count": count}
-        )
-        return await _get_emotes(self.cur)
-
-
-async def _get_emotes(cur) -> List[PartialEmoji]:
-    results = await cur.fetchall()
-    return [
-        PartialEmoji(animated=animated, name=name.rstrip(" "), id=id)
-        for animated, name, id in results
-    ]
