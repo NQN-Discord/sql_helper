703a471c119ab83b19e1eb6c5ce0cd30fee25417
Blue <blue@nqn.blue>
2025-08-27 20:31:40 +0100

    Add sentry span

--- sql_helper/metrics.py
+++ sql_helper/metrics.py
@@ -1,6 +1,6 @@
 import time
 from prometheus_client import Histogram
-from sentry_sdk import add_breadcrumb
+from sentry_sdk import add_breadcrumb, start_span
 
 
 def sql_wrapper(namespace: str):
@@ -10,22 +10,26 @@ def sql_wrapper(namespace: str):
 
     def middle(execute):
         async def execute_wrapper(query, *args, **kwargs):
-            start_time = time.perf_counter()
-            try:
-                return await execute(query, *args, **kwargs)
-            finally:
-                total_time = time.perf_counter() - start_time
-                sql_request.labels(query=query).observe(total_time)
+            with start_span(
+                op="postgres",
+                name=query,
+            ):
+                start_time = time.perf_counter()
+                try:
+                    return await execute(query, *args, **kwargs)
+                finally:
+                    total_time = time.perf_counter() - start_time
+                    sql_request.labels(query=query).observe(total_time)
 
-                add_breadcrumb(
-                    message="Made SQL query",
-                    category="sql",
-                    data={
-                        "query": query,
-                        "params": kwargs.get("parameters"),
-                        "total_time": total_time,
-                    },
-                )
+                    add_breadcrumb(
+                        message="Made SQL query",
+                        category="sql",
+                        data={
+                            "query": query,
+                            "params": kwargs.get("parameters"),
+                            "total_time": total_time,
+                        },
+                    )
 
         return execute_wrapper
 
